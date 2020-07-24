c52312407d43eddaf09bd323ac4f65cfb2ba6e75
Steven Conaway <steven.conaway@icloud.com>
2020-07-23 19:09:23 -0700

    test: test all icons in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import { StyleSheet, View, StatusBar, Button } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  Button,
+  ScrollView,
+  Dimensions,
+  SafeAreaView,
+} from 'react-native';
 import CreditCardDisplay from 'react-native-credit-card-display';
 
 export default function App() {
@@ -7,30 +15,94 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <SafeAreaView />
       <StatusBar barStyle="dark-content" />
-      <View style={styles.cardContainer}>
-        <CreditCardDisplay
-          number={4242424242424242}
-          cvc={123}
-          expiration="06/21"
-          name="John J. Doe"
-        />
-      </View>
-      <View style={styles.cardContainer}>
-        <CreditCardDisplay
-          number={'3088 0000 0000 0009'}
-          cvc={123}
-          expiration="06/21"
-          name="John J. Doe"
-          since={2004}
-          height={126}
-          width={200}
-          fontSize={12}
-          friction={10}
-          flipped={card2Flipped}
-        />
-        <Button onPress={() => setCard2Flipped(!card2Flipped)} title="Flip" />
-      </View>
+      <ScrollView
+        contentContainerStyle={{ alignContent: 'center' }}
+        style={{
+          width: Dimensions.get('screen').width,
+        }}
+      >
+        <View style={styles.cardContainer}>
+          <CreditCardDisplay
+            number={4242424242424242}
+            cvc={123}
+            expiration="06/21"
+            name="John J. Doe"
+          />
+        </View>
+        <View style={styles.cardContainer}>
+          <CreditCardDisplay
+            number={'3088 0000 0000 0009'}
+            cvc={123}
+            expiration="06/21"
+            name="John J. Doe"
+            since={2004}
+            height={126}
+            width={200}
+            fontSize={12}
+            friction={10}
+            flipped={card2Flipped}
+          />
+          <Button onPress={() => setCard2Flipped(!card2Flipped)} title="Flip" />
+        </View>
+        <View style={styles.cardContainer}>
+          <CreditCardDisplay
+            number={4026424242424242}
+            cvc={123}
+            expiration="06/21"
+            name="John J. Doe"
+          />
+        </View>
+        <View style={styles.cardContainer}>
+          <CreditCardDisplay
+            number={'3400 0000 0000 009'}
+            cvc={123}
+            expiration="06/21"
+            name="John J. Doe"
+          />
+        </View>
+        <View style={styles.cardContainer}>
+          <CreditCardDisplay
+            number={5018424242424242}
+            cvc={123}
+            expiration="06/21"
+            name="John J. Doe"
+          />
+        </View>
+        <View style={styles.cardContainer}>
+          <CreditCardDisplay
+            number={5223424242424242}
+            cvc={123}
+            expiration="06/21"
+            name="John J. Doe"
+          />
+        </View>
+        <View style={styles.cardContainer}>
+          <CreditCardDisplay
+            number={6042424242424242}
+            cvc={123}
+            expiration="06/21"
+            name="John J. Doe"
+          />
+        </View>
+        <View style={styles.cardContainer}>
+          <CreditCardDisplay
+            number={6242424242424242}
+            cvc={123}
+            expiration="06/21"
+            name="John J. Doe"
+          />
+        </View>
+        <View style={styles.cardContainer}>
+          <CreditCardDisplay
+            number={3542424242424242}
+            cvc={123}
+            expiration="06/21"
+            name="John J. Doe"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -42,6 +114,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   cardContainer: {
-    marginVertical: 50,
+    marginVertical: 25,
+    alignItems: 'center',
   },
 });
